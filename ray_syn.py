396e75dca963261d23d7037a40f7991184d5a073
xinyandai <381675152@qq.com>
2019-01-13 10:37:47 +0800

    Fix bug by shuffle dataset with seed in each worker.

--- ray_syn.py
+++ ray_syn.py
@@ -17,7 +17,7 @@ from quantizer_codebook import CodebookQuantizer
 Quantizer = None
 parser = argparse.ArgumentParser(description="Run the synchronous parameter "
                                              "server example.")
-parser.add_argument("--num-workers", default=4, type=int,
+parser.add_argument("--num-workers", default=1, type=int,
                     help="The number of workers to use.")
 parser.add_argument("--redis-address", default=None, type=str,
                     help="The Redis address of the cluster.")
@@ -29,18 +29,21 @@ parser.add_argument("--dataset", default="cifar", type=str,
                     help="Using 2-phases quantization.")
 parser.add_argument("--batch-size", default=128, type=int,
                     help="Using 2-phases quantization.")
+parser.add_argument("--test-batch-size", default=1000, type=int,
+                    help="Using 2-phases quantization.")
 
 
-def load_network(args):
+def load_network(args, seed=0, validation=False):
     if args.dataset == 'mnist':
-        dataset = mpi_dataset.download_mnist_retry(0)
+        dataset = mpi_dataset.download_mnist_retry(seed)
         network = SimpleCNN
     elif args.dataset == 'cifar':
-        dataset = mpi_dataset.download_cifar10_retry(0)
+        dataset = mpi_dataset.download_cifar10_retry(seed)
         network = ResNet
     else:
         assert False
-    return network(dataset=dataset, batch_size=args.batch_size)
+    return network(dataset=dataset,
+                   batch_size=args.test_batch_size if validation else args.batch_size)
 
 
 @ray.remote
@@ -66,7 +69,7 @@ class ParameterServer(object):
 class Worker(object):
     def __init__(self, args, worker_index):
         self.worker_index = worker_index
-        self.net = load_network(args)
+        self.net = load_network(args, seed=worker_index)
         self.batch_size = args.batch_size
         self.dataset = self.net.dataset
         self.quantizer = Quantizer(self.net.variables.placeholders)
@@ -91,17 +94,7 @@ if __name__ == "__main__":
 
     ray.init(redis_address=args.redis_address)
 
-    test_batch_size = 1000
-
-    if args.dataset == 'mnist':
-        dataset = mpi_dataset.download_mnist_retry(0)
-        network = SimpleCNN
-    elif args.dataset == 'cifar':
-        dataset = mpi_dataset.download_cifar10_retry(0)
-        network = ResNet
-    else:
-        assert False
-    net = network(dataset=dataset, batch_size=test_batch_size)
+    valid_net = load_network(args, seed=0, validation=True)
 
     # dataset, network, batch_size, two_phases=False
     ps = ParameterServer.remote(args, two_phases=args.two_phases)
@@ -123,8 +116,8 @@ if __name__ == "__main__":
 
             if i % 10 == 0:
                 # Evaluate the current model.
-                net.variables.set_flat(ray.get(current_weights))
-                test_xs, test_ys = dataset.test.next_batch(test_batch_size)
-                loss, accuracy = net.compute_loss_accuracy(test_xs, test_ys)
+                valid_net.variables.set_flat(ray.get(current_weights))
+                test_xs, test_ys = valid_net.dataset.test.next_batch(args.test_batch_size)
+                loss, accuracy = valid_net.compute_loss_accuracy(test_xs, test_ys)
                 print("%d, %.3f, %.3f, %.3f" % (i, timer.toc(), loss, accuracy))
             i += 1
