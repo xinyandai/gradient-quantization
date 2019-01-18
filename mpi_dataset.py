e3308b010ba570ff0bf6ea1847663774613d7182
xinyandai <381675152@qq.com>
2019-01-18 12:02:50 +0800

    Add distort.

--- mpi_dataset.py
+++ mpi_dataset.py
@@ -15,13 +15,14 @@ class Datasets(object):
 
 
 class BatchDataset(object):
-    def __init__(self, x, y, num_classes=-1, seed=0, one_hot=True):
+    def __init__(self, x, y, num_classes=-1, seed=0, one_hot=True, distort=False):
         assert len(x) == len(y)
 
         self.i = 0
         self.size = len(x)
-
         self.x = x.copy()
+
+        _, self.width, self.height, self.channels = np.shape(self.x)
 
         if one_hot:
             if num_classes == -1:
@@ -31,17 +32,52 @@ class BatchDataset(object):
             self.y = y.copy()
 
         np.random.seed(seed)
-
         self.idx = np.arange(self.size)
         np.random.shuffle(self.idx)
-
         self.x[:] = self.x[self.idx]
         self.y[:] = self.y[self.idx]
+
+        if distort:
+            self.distort = distort
+            self.x_ref = self.x.copy()
+            self.distorted_image = self._distort_ops()
+            self.distort_sess = tf.Session()
+
+    def distort_images(self):
+        self.x = self.distort_sess.run(self.distorted_image, feed_dict={self.image: self.x_ref})
+
+    def _distort_ops(self):
+        # Image processing for training the network. Note the many random
+        # distortions applied to the image.
+        self.image = tf.placeholder(tf.float32, [None, self.width, self.height, self.channels])
+        # Randomly crop a [height, width] section of the image.
+        distorted_image = tf.map_fn(
+            lambda img: tf.random_crop(img, [self.height, self.width, self.channels]), self.image)
+        # Randomly flip the image horizontally.
+        distorted_image = tf.map_fn(
+            lambda img: tf.image.random_flip_left_right(img), distorted_image)
+        # Because these operations are not commutative, consider randomizing
+        # the order their operation.
+        distorted_image = tf.map_fn(
+            lambda img: tf.image.random_brightness(img, max_delta=63), distorted_image
+        )
+        distorted_image = tf.map_fn(
+            lambda img: tf.image.random_contrast(img, lower=0.2, upper=1.8), distorted_image
+        )
+        # Subtract off the mean and divide by the variance of the pixels.
+        # float_image = tf.image.per_image_whitening(distorted_image)
+        float_image = tf.map_fn(
+            lambda img: tf.image.per_image_standardization(img), distorted_image
+        )
+
+        return float_image
 
     def next_batch(self, batch_size):
         assert batch_size > 0
         batch = (self.x[self.i:self.i + batch_size, :], self.y[self.i: self.i + batch_size, :])
         if self.i + batch_size >= self.size:
+            if self.distort:
+                self.distort_images()
             self.i = batch_size + self.i - self.size
             padding = (self.x[0:self.i, :], self.y[0: self.i, :])
             return (
@@ -58,9 +94,9 @@ def download_mnist_retry(seed):
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
     return Datasets(
-        BatchDataset(np.expand_dims(x_train, axis=3), y_train, num_classes, seed),
-        BatchDataset(np.expand_dims(x_train, axis=3), y_train, num_classes, seed),
-        BatchDataset(np.expand_dims(x_test, axis=3), y_test, num_classes, seed)
+        BatchDataset(np.expand_dims(x_train, axis=3), y_train, num_classes, seed, distort=True),
+        BatchDataset(np.expand_dims(x_train, axis=3), y_train, num_classes, seed, distort=False),
+        BatchDataset(np.expand_dims(x_test, axis=3), y_test, num_classes, seed, distort=False)
     )
 
 
@@ -71,7 +107,7 @@ def download_cifar10_retry(seed):
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
     return Datasets(
-        BatchDataset(x_train, y_train.flatten(), num_classes, seed),
-        BatchDataset(x_train, y_train.flatten(), num_classes, seed),
-        BatchDataset(x_test, y_test.flatten(), num_classes, seed)
+        BatchDataset(x_train, y_train.flatten(), num_classes, seed, distort=True),
+        BatchDataset(x_train, y_train.flatten(), num_classes, seed, distort=False),
+        BatchDataset(x_test, y_test.flatten(), num_classes, seed, distort=False)
     )
