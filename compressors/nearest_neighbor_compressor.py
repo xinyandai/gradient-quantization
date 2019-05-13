Modify default parameters in main. Handle the case that dimension is not dividable in QSGD and NNQ. Divide the gradient by the maximum item rather than norm. Fix one bug in Probabilistic Compressor.

compressors/nearest_neighbor_compressor.py
@@ -13,19 +13,41 @@ class NearestNeighborCompressor(object):
         n_bit = args.n_bit
         compressed_norm = n_bit != 32
         assert c_dim > 0
-        assert k_bit > 0
+        assert k_bit >= 0
         assert n_bit > 0
 
         self.cuda = not args.no_cuda
         self.size = size
         self.shape = shape
-        self.dim = c_dim if c_dim < size else size
-        self.K = 2 ** k_bit
 
+        if c_dim == 0 or self.size < args.c_dim:
+            self.dim = self.size
+        else:
+            self.dim = c_dim
+            for i in range(0, 10):
+                if size % self.dim != 0:
+                    self.dim = self.dim // 2 * 3
+
+        if c_dim != self.dim:
+            print("alternate dimension form"
+                  " {} to {}, size {} shape {}"
+                  .format(c_dim, self.dim, size, shape))
+
+        assert size % self.dim == 0, \
+            "not divisible size {}  c_dim {} self.dim {}"\
+                .format(size, c_dim, self.dim)
+
+        if k_bit <= 0:
+            self.K = self.dim
+        else:
+            self.K = 2 ** k_bit
+        # self.codewords = normalize(np.random.normal(size=(self.K, self.dim)))[1].astype(np.float32)
         if self.K == self.dim:
             self.codewords = stats.ortho_group.rvs(self.dim).astype(np.float32)
+            # self.codewords = np.identity(self.dim, dtype=np.float32)
+            # self.codewords = normalize(np.random.normal(size=(self.dim, self.dim)))[1].astype(np.float32)
         else:
-            location = './codebook/angular_dim_{}_Ks_{}.fvecs'.format(self.dim, self.K)
+            location = './new_codebook/angular_dim_{}_Ks_{}.fvecs'.format(self.dim, self.K)
             _, self.codewords = normalize(fvecs_read(location))
 
         self.codewords = torch.from_numpy(self.codewords)

compressors/probabilistic_compressor.py
@@ -5,7 +5,7 @@ class ProbabilisticCompressor(object):
     def __init__(self, n_bit, args):
         self.s = 2 ** n_bit
         self.cuda = not args.no_cuda
-        self.code_dtype = torch.uint8 if n_bit <= 8 else torch.int32
+        self.code_dtype = torch.int32
         self.random = args.random
 
 
@@ -13,7 +13,7 @@ class ProbabilisticCompressor(object):
         lower_bound = torch.min(vec)
         upper_bound = torch.max(vec)
         if lower_bound - upper_bound == 0.0:
-            return lower_bound, upper_bound, torch.zeros_like(vec).astype(torch.int32)
+            return lower_bound, upper_bound, torch.zeros_like(vec).type(self.code_dtype)
         scaled_vec = torch.abs((vec - lower_bound) / (upper_bound - lower_bound)) * self.s
         l = torch.clamp(scaled_vec, 0, self.s-1).type(dtype=self.code_dtype)
 
@@ -23,11 +23,11 @@ class ProbabilisticCompressor(object):
             r = torch.rand(l.size())
             if self.cuda:
                 r = r.cuda()
-            l[:] += probabilities > r
+            l[:] += (probabilities > r).type(self.code_dtype)
         return lower_bound, upper_bound, l
 
     def decompress(self, signature):
         [lower_bound, upper_bound, l] = signature
         scaled_vec = l.type(dtype=torch.float32)
-        compressed = scaled_vec / self.s * (upper_bound - lower_bound) + lower_bound
+        compressed = scaled_vec  * (upper_bound - lower_bound) / self.s + lower_bound
         return compressed

compressors/qsgd_compressor.py
@@ -5,18 +5,39 @@ class QSGDCompressor(object):
     def __init__(self, size, shape, args):
         self.random = args.random
         self.bit = args.n_bit
+        c_dim = args.c_dim
         assert self.bit > 0
 
         self.cuda = not args.no_cuda
         self.s = 2 ** self.bit
         self.size = size
         self.shape = shape
-        self.dim = args.c_dim if self.size >= (4096*2) else self.size
+
+        if c_dim == 0 or self.size < args.c_dim:
+            self.dim = self.size
+        else:
+            self.dim = c_dim
+            for i in range(0, 10):
+                if size % self.dim != 0:
+                    self.dim = self.dim // 2 * 3
+
+        if c_dim != self.dim:
+            print("alternate dimension form"
+                  " {} to {}, size {} shape {}"
+                  .format(c_dim, self.dim, size, shape))
+
+        assert self.dim != 0, \
+            "0 sub dimension size {}  " \
+            "c_dim {} self.dim {}"\
+                .format(size, c_dim, self.dim)
+        assert size % self.dim == 0, \
+            "not divisible size {} " \
+            " c_dim {} self.dim {}"\
+                .format(size, c_dim, self.dim)
+
         self.M = size // self.dim
         self.code_dtype = torch.uint8 if self.bit <= 8 else torch.int32
-        assert size % self.dim == 0, \
-            "dimension of variable {} {} should be smaller than " \
-            "{} or dividable by {}".format(shape, size, self.dim, self.dim)
+
 
     def compress(self, vec):
         """
@@ -24,7 +45,8 @@ class QSGDCompressor(object):
         :return: norm, signs, quantized_intervals
         """
         vec = vec.view(-1, self.dim)
-        norm = torch.norm(vec, dim=1, keepdim=True)
+        # norm = torch.norm(vec, dim=1, keepdim=True)
+        norm = torch.max(torch.abs(vec), dim=1, keepdim=True)[0]
         normalized_vec = vec / norm
 
         scaled_vec = torch.abs(normalized_vec) * self.s