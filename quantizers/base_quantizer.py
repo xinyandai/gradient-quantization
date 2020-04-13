ad32666f5eb353e866727df16bd97f218ed3616d
xinyan <xinyan.dai@outlook.com>
2020-04-13 12:55:35 +0800

    Dev double-squeeze.

--- quantizers/base_quantizer.py
+++ quantizers/base_quantizer.py
@@ -9,6 +9,7 @@ class Quantizer():
         self.compressed_gradients = [list() for _ in range(self.num_layers)]
         self.args = args
         self.error_feedback = args.ef
+        self.two_phase = self.args.two_phase
         for param in self.parameters:
             param_size = param.flatten().shape[0]
             self.compressors.append(
@@ -18,26 +19,42 @@ class Quantizer():
             if self.error_feedback:
                 param.error = [torch.zeros_like(param)
                                for _ in range(args.num_users)]
+            if self.error_feedback and self.two_phase:
+                param.server_error = torch.zeros_like(param)
 
     def record(self, user):
         for i, param in enumerate(self.parameters):
             if self.error_feedback:
                 param.grad.data.add_(param.error[user])
-            decompressed_g = self.compressors[i].decompress(
-                self.compressors[i].compress(param.grad.data)
-            )
-            self.compressed_gradients[i].append(decompressed_g)
-            if self.error_feedback:
+                decompressed_g = self.compressors[i].decompress(
+                    self.compressors[i].compress(param.grad.data)
+                )
                 param.error[user].data = param.grad.data - decompressed_g
+            else:
+                decompressed_g = self.compressors[i].decompress(
+                    self.compressors[i].compress(param.grad.data)
+                )
+            self.compressed_gradients[i].append(decompressed_g)
+
+
 
     def apply(self):
         for i, param in enumerate(self.parameters):
             g = torch.stack(self.compressed_gradients[i], dim=0).mean(dim=0)
 
-            # if compress gradient on two phase, i.e., compress the sum of decompressed gradient
-            if self.args.two_phase:
-                g = self.compressors[i].decompress(
+            # if compress gradient on two phase, i.e.,
+            # compress the sum of decompressed gradient
+            if self.two_phase:
+                if self.error_feedback:
+                    g.add_(param.server_error)
+                    decompressed_g = self.compressors[i].decompress(
                         self.compressors[i].compress(g))
+                    param.server_error = g - decompressed_g
+                    g = decompressed_g
+                else:
+                    g = self.compressors[i].decompress(
+                        self.compressors[i].compress(g))
+
             param.grad.data = g
         for compressed in self.compressed_gradients:
             compressed.clear()
